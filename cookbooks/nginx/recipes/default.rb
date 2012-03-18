271289aeba843395e35872b627d8b602d5c61687
vagmi <vagmi.mudumbai@gmail.com>
2012-03-18 12:03:34 +0530

    add basic cookbooks for manging our nodes

---
+++ cookbooks/nginx/recipes/default.rb
@@ -0,0 +1,13 @@
+#
+# Cookbook Name:: nginx
+# Recipe:: default
+#
+# Copyright 2012, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+package 'nginx'
+service 'nginx' do
+  supports :status=>true
+  action :start
+end
